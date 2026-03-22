a5fe4e5e8f5ba67255484f0cbc19216b0596bba6
Saura-4 <saurav04042004@gmail.com>
2026-03-22 22:49:17 +0530

    Remove publish artifacts from repo

--- .dist/publish-v1.2/src/background/core/storage.js
+++
@@ -1,9 +0,0 @@
-export function getStorage(keys) {
-    return new Promise((resolve) => {
-        try {
-            chrome.storage.local.get(keys, (items) => resolve(items || {}));
-        } catch (error) {
-            resolve({});
-        }
-    });
-}
