611dcdb720b4c9a19db47f8fd85c04376ee11008
Saura-4 <saurav04042004@gmail.com>
2026-03-22 22:40:54 +0530

    Remove staged publish artifacts

--- .dist/publish-v1.2/src/background/background.js
+++
@@ -1,5 +0,0 @@
-import { registerChromeUiHandlers } from './handlers/chrome-ui.js';
-import { createRuntimeMessageListener } from './handlers/runtime-router.js';
-
-registerChromeUiHandlers();
-chrome.runtime.onMessage.addListener(createRuntimeMessageListener());
